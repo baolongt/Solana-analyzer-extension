23fc0fba6f3572b2164b77f3d436a89ebc8e3d3c
Ducbii4202 <144377938+Ducbii4202@users.noreply.github.com>
2024-05-01 23:10:29 +0700

    feat: message

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -4,9 +4,10 @@ import useStorage from '@src/shared/hooks/useStorage';
 import exampleThemeStorage from '@src/shared/storages/exampleThemeStorage';
 import withSuspense from '@src/shared/hoc/withSuspense';
 import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
-import { analyzeArguments } from './analyzeArguments';
+// import { analyzeArguments } from './analyzeArguments';
 
 const Popup = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const theme = useStorage(exampleThemeStorage);
 
   const [data, setData] = useState<{
@@ -51,21 +52,27 @@ const Popup = () => {
     );
   };
 
-  const handleDecode = () => {
-    console.log('data', data);
-    analyzeArguments(data.data);
-  };
+  // const handleDecode = () => {
+  //   console.log('data', data);
+  //   analyzeArguments(data.data);
+  // };
 
   return (
-    <div
-      className="App"
-      style={{
-        backgroundColor: theme === 'light' ? '#fff' : '#000',
-      }}>
-      <h1>{data ? JSON.stringify(data) : 'No Data'}</h1>
-      <button onClick={() => handleReject()}>no</button>
-      <button onClick={() => handleApprove()}>yes</button>
-      <button onClick={() => handleDecode()}>decode</button>
+    <div className="content">
+      {/* <h1 className='data'>{data ? JSON.stringify(data) : 'No Data'}</h1> */}
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam fugiat ab architecto similique et optio ipsum,
+        repudiandae vitae molestias! At natus dignissimos molestiae, error eligendi vitae minus. Saepe, ducimus totam.
+      </h1>
+      <div className="button-container">
+        <button className="button no" onClick={() => handleReject()}>
+          No
+        </button>
+        <button className="button yes" onClick={() => handleApprove()}>
+          Yes
+        </button>
+        {/* <button className="button decode" onClick={() => handleDecode()}>Decode</button> */}
+      </div>
     </div>
   );
 };
